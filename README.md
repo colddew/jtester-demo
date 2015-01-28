22877052af98f26f8c9e0980c536de7bf2230f60
colddew <88028842@qq.com>
2015-01-28 22:34:07 +0800

    add database script

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ usage
 
 - fork or clone this project directly
 - modify jtester.properties and configure your own data source
+- execute user2.sql script in sqlserver
 - right click testng.xml and run as testng suite
---
+++ sql/user2.sql
@@ -0,0 +1,11 @@
+-- ----------------------------
+-- Table structure for user2
+-- ----------------------------
+CREATE TABLE user2 (
+id int NOT NULL IDENTITY(1,1) ,
+username varchar(32) NULL ,
+password varchar(32) NULL ,
+datee datetime NULL ,
+bd decimal(32,2) NULL 
+)
+GO
