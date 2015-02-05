4709b5fdead30e8a932bbe7dc0a9eea3d5d7db69
colddew <88028842@qq.com>
2015-02-05 21:05:01 +0800

    update to jtester 1.1.10; add groups test

---
+++ src/test/java/cn/qfc/bean/SuiteTest3.java
@@ -0,0 +1,20 @@
+package cn.qfc.bean;
+
+import org.jtester.testng.JTester;
+import org.testng.annotations.Test;
+
+@Test(groups={"g3"})
+public class SuiteTest3 extends JTester {
+	
+	public void testDemo1() {
+		System.out.println("############### jtester demo31 ###############");
+	}
+	
+	public void testDemo2() {
+		System.out.println("############### jtester demo32 ###############");
+	}
+	
+	public void testDemo3() {
+		System.out.println("############### jtester demo33 ###############");
+	}
+}
